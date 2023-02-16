67ced04b926671574e33dfb6126398d1cb96bf68
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-16 08:49:00 -0800

    Hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - prints "Programming is like building a multilingual
- * puzzle, followed by a newline."
- * Return: 0.(Success)
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingualpuzzle");
-	return(0);
+	puts("\"Programming is like building a multilingualpuzzle");	
+	return (0);
 }
