ea4f0f4b668795435d412024aaa1dd0cfcbc0819
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-18 14:17:09 -0800

    Print alphabet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,15 +17,15 @@ int main(void)
 	/* your code goes there */
 	if (a > 5)
 	{
-		printf("a of %d is %d and is greater than 5\n", n, a);
-	}
-	else if (a < 6 && a != 0)
-	{
-		printf("a of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	}
 	else if (a == 0)
 	{
-		printf("a of %d is %d and is 0\n", n, a);
+		printf("Last digit of %d is %d and is 0\n", n, a);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
 }
