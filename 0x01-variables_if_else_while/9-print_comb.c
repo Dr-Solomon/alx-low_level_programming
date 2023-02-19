eab4ab4a3a2d81cee723cecacc3d36d5092a0093
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-18 18:12:15 -0800

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,26 +6,16 @@
  */
 int main(void)
 {
-	int x, y, z;
+	int a;
 
-	for (x = '0'; x < '9'; x++)
+	for (a = 38; a < 48; a++)
 	{
-	for (y = x + 1; y <= '9'; y++)
-	{
-	for (z = y + 1; z <= '9'; z++)
-	{
-		if ((x != y) != z)
-	{
-		putchar(x);
-		putchar(y);
-		putchar(z);
-		if (x == '7' && y == '8')
-			continue;
-		putchar(',');
-		putchar(' ');
-	}
-	}
-	}
+		putchar(a);
+		if (a != 47)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	 putchar('\n');
 	return (0);
