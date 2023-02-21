3f92ea27e9311a9e2d6522f9af570f299ea0b34a
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-21 15:19:47 -0800

    Compute absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute the absolute value of an intiger
+ * @n: the intiger in query
+ *
+ * Return: the abs of the int.
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
