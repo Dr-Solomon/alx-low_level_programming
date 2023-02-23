2865277afd47fffe7f80d061e627fc9c77d652f6
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-23 12:05:15 -0800

    leap year

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,16 +16,11 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > c && c > b)
+	else if (a >= b && a >= c)
 		largest = a;
-	else if (b > a && a > c)
-	{
-		largest = b;
 	}
-	else if (b > c && c > a)
+	else if (b >= a && b >= c)
 		largest = b;
-	else if (c > a && a > b)
-		largest = c;
 	else
 	{
 		largest = c;
