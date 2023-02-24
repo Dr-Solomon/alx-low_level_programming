5a36f2a322686f2249c52a6b7ecc560ceb063e14
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-24 11:26:39 -0800

    Draws diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: numbers of '\' to be printed
+ */
+void print_diagonal(int n)
+{
+	int a, b;
+
+	if (n <= 0)
+		_putchar('\n');
+}
