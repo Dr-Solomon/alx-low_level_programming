c0a02ea14d2ab50cd2018cceb5e11bcc137532b3
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-24 17:26:41 -0800

    Draws square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,21 +2,22 @@
 
 /**
  * print_square - draws a square shape
- * @size: size of the square
+ * @size:  square made of '#'
  */
 void print_square(int size)
 {
+	int i, j;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int i, j;
-
 		for (i = 1; i < size; i++)
 		{
-			_putchar('#');
+			_putchar('#')
+				;
 			for (j = 2; j < size; j++)
 			{
 				-putchar('#');
