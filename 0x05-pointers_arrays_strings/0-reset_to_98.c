ee39b05eb5340105865795c792da03bd52de65b9
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-02-27 00:34:26 -0800

    Takes pointer to an integer

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,9 +2,9 @@
 
 /**
  * reset_to_98 ->a function that takes a pointer to an integer as parameter
- * @: pointer to n to be updated
+ * @n: pointer to n to be updated
  */
-void reset_to_98(int *n);
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
