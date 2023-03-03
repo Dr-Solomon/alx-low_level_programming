dfe6af524dbeb470e2550d1497691d2f6f7d1d27
Dr-Solomon <abebesolomon2022gc@gmail.com>
2023-03-03 14:42:25 -0800

    Prints buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -9,35 +9,40 @@
  */
 void print_buffer(char *b, int size)
 {
-	int byte, index;
+	int k, i, j;
 
-	if (byte = 0; byte < size; byte += 10)
-	{
-		printf("%08x:", byte);
+	k = 0;
 
-		for (index = 0; index < 10; index++)
-		{
-			if ((index + byte) >= size)
-				printf(" ");
-			else
-				printf("%02x:", *(b + index + byte))
-
-			if ((index % 2) != 0 && index != 0)
-				printf(" ");
-		}
-		for (index = 0; index < 10; idex++)
-		{
-			if ((index + byte) >= size)
-				break;
-			else if (*(b + index + byte) > 31 && *(b + index + byte) <= 126)
-				printf("%c", *(b + index + byte));
-			else
-				printf(".");
-		}
-		if (byte >= size)
-			continue;
-		printf("\n")'
-	}
 	if (size <= 0)
+	{
 		printf("\n");
+		return;
+
+		while (k < size)
+		{
+			j = size - k < 10 ? size - k : 10;
+				printf("%08x:", k);
+			for (i = 0; i < 10; i++)
+			{
+				if (i < j)
+
+					printf("%02x", *(b + k + i));
+				else
+					printf(" ");
+				if (i % 2)
+				{
+					printf(" ");
+				}
+			}
+			for (i = 0; i < j; i++)
+				int c = *(b + k + i);
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
+		}
+		printf("\n");
+		k += 10;
+	}
 }
